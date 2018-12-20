162ddd87733d028ff160ad6e9450c8818f48be2c
Naman <naman7kr@gmail.com>
2018-12-20 18:34:30 +0530

    connected to firebase....

--- app/src/main/java/com/chat/pcon/groupmessenger/LoginActivity.java
+++ app/src/main/java/com/chat/pcon/groupmessenger/LoginActivity.java
@@ -1,19 +1,31 @@
 package com.chat.pcon.groupmessenger;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     TextInputEditText email,password;
     Button cancel,submit;
     TextView register;
     RelativeLayout rlayout;
+    FirebaseAuth auth;
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         onSubmit();
         onCancel();
         onRegister();
+
+        auth = FirebaseAuth.getInstance();
     }
     void init(){
         email = findViewById(R.id.login_email);
@@ -31,14 +45,39 @@ public class LoginActivity extends AppCompatActivity {
         submit = findViewById(R.id.login_submit);
         register = findViewById(R.id.login_register);
         rlayout = findViewById(R.id.login_rel);
+        auth = FirebaseAuth.getInstance();
+    }
+    void updateUI(FirebaseUser user){
+        if(user!=null){
+            Intent intent = new Intent(this,ChatActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
     void onSubmit(){
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),ChatActivity.class);
-                startActivity(intent);
-                //finish();
+                if (email.getText().toString().compareTo("")!=0  && password.getText().toString().compareTo("")!=0) {
+                    auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = auth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        updateUI(null);
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
@@ -65,5 +104,12 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
     }
 }
--- app/src/main/java/com/chat/pcon/groupmessenger/RegisterActivity.java
+++ app/src/main/java/com/chat/pcon/groupmessenger/RegisterActivity.java
@@ -1,25 +1,36 @@
 package com.chat.pcon.groupmessenger;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     Button register;
     TextInputEditText name,email,password;
+    FirebaseAuth mAuth;
 
+    private static final String TAG = "RegisterActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         init();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        mAuth = FirebaseAuth.getInstance();
         onRegister();
     }
     void init(){
@@ -32,10 +43,36 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(email.getText().toString().compareTo("")!=0 && password.getText().toString().compareTo("")!=0) {
+                    mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        updateUI(null);
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
+
+    private void updateUI(FirebaseUser user) {
+        if(user!=null){
+
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
